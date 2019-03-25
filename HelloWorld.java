23aea5abb2767024c6635dfc993d332df3e76b3a
Timbo1967 <37069644+Timbo1967@users.noreply.github.com>
2019-03-25 16:41:29 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
  public class HelloWorld {
    public static void main(String[]args){
-    System.out.println("------********* HelloWorld & welcome to Jenkins Training Timbo it has a been a struggle *********---------");
+    System.out.println("------** HelloWorld & welcome to Jenkins Training Timbo it has a been a struggle many times over---------");
    }
  }  
